918c4cab0672fc510228c70b9c26edf500c47d44
ob1 <brandonong@aisingapore.org>
2024-06-23 06:35:21 -0700

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncio
 from typing import Union
 
 from dotenv import load_dotenv
-import openai
+from openai import AzureOpenAI
 from deepgram import Deepgram
 import pygame
 from pygame import mixer
